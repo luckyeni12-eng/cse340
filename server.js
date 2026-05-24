e5963e65652e78a8b2aaee638f7474f6e7b5ae83
Lucky Eni <realchartwitluckyenison12>
2026-05-24 03:18:02 +0100

    well

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 });
 
 /* =========================
-   REQUEST LOGGER
+   REQUEST LOGGERs
 ========================= */
 app.use((req, res, next) => {
   console.log(`Incoming request: ${req.method} ${req.url}`);
